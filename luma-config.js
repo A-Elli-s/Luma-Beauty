fb9dacb2496a9234432615da0b18e7ed33ba454b
A-Elli-s <abrownellis06@gmail.com>
2026-09-11 09:52:28 -0400

    Add creator backend configuration

---
+++ luma-config.js
@@ -0,0 +1,5 @@
+window.LUMA_CONFIG = {
+  // Paste the deployed creator API URL here after the Supabase backend is connected.
+  // Example: https://YOUR_PROJECT.supabase.co/functions/v1/creator-api
+  creatorApiUrl: ''
+};
