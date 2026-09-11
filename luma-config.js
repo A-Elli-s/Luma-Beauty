cd5d097188bf13f66d2ebb913b4fcfeacc8c6222
A-Elli-s <abrownellis06@gmail.com>
2026-09-11 10:01:40 -0400

    Connect Luma creator system to live Supabase API

--- luma-config.js
+++ luma-config.js
@@ -1,5 +1,3 @@
 window.LUMA_CONFIG = {
-  // Paste the deployed creator API URL here after the Supabase backend is connected.
-  // Example: https://YOUR_PROJECT.supabase.co/functions/v1/creator-api
-  creatorApiUrl: ''
+  creatorApiUrl: 'https://biwuctqlmcxscwpwdyij.supabase.co/functions/v1/creator-api'
 };
